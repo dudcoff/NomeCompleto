3b367f573f2e07b904754ea9bd916989093f0d41
Maria Eduarda Casalunga <mariaeduardacasalunga@gmail.com>
2022-03-26 20:11:04 -0300

    finaliza exercício

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string nome, sobrenome, sobrenomeEmMaiusculo;
+
+Console.Write("Digite seu primeiro nome:");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome:");
+sobrenome = Console.ReadLine()!;
+
+sobrenomeEmMaiusculo = sobrenome.ToUpper();
+
+Console.WriteLine($"Nome Completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome Catálogo: {sobrenomeEmMaiusculo}, {nome}");
+
+
+
